8f1ff07384f6fd8c5bfb2ea8d444b3fe32a9f721
xuxiaocheng <2762267080@qq.com>
2025-01-13 01:32:26 +0800

    feat(main): 添加get_hitokoto_by_id

--- src/bundles.rs
+++ src/bundles.rs
@@ -18,3 +18,20 @@ include!(concat!(env!("OUT_DIR"), "/sentences_i.rs"));
 include!(concat!(env!("OUT_DIR"), "/sentences_j.rs"));
 include!(concat!(env!("OUT_DIR"), "/sentences_k.rs"));
 include!(concat!(env!("OUT_DIR"), "/sentences_l.rs"));
+
+/// Get hitokoto by id. If not found, return `None`.
+pub fn get_hitokoto_by_id(id: u32) -> Option<Hitokoto> {
+    for h in HITOKOTOS_A { if h.id == id { return Some(h.clone()) } }
+    for h in HITOKOTOS_B { if h.id == id { return Some(h.clone()) } }
+    for h in HITOKOTOS_C { if h.id == id { return Some(h.clone()) } }
+    for h in HITOKOTOS_D { if h.id == id { return Some(h.clone()) } }
+    for h in HITOKOTOS_E { if h.id == id { return Some(h.clone()) } }
+    for h in HITOKOTOS_F { if h.id == id { return Some(h.clone()) } }
+    for h in HITOKOTOS_G { if h.id == id { return Some(h.clone()) } }
+    for h in HITOKOTOS_H { if h.id == id { return Some(h.clone()) } }
+    for h in HITOKOTOS_I { if h.id == id { return Some(h.clone()) } }
+    for h in HITOKOTOS_J { if h.id == id { return Some(h.clone()) } }
+    for h in HITOKOTOS_K { if h.id == id { return Some(h.clone()) } }
+    for h in HITOKOTOS_L { if h.id == id { return Some(h.clone()) } }
+    None
+}
